e86504ed96d2841fa75608ac6a6912f03d8186bd
Denys Maletskiy <maletsden@gmail.com>
2017-06-16 12:19:31 +0300

    Flex main page

--- pages/main.php
+++ pages/main.php
@@ -1,47 +1,46 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>slippry demo</title>
-		<script src="js/full_width_slider/slippry.min.js"></script>
-		<meta name="viewport" content="width=device-width">
-    <link rel="stylesheet" href="css/slippry.css">
-	</head>
-	<body>
-		<section class="demo_wrapper">
-			<article class="demo_block">
-			<ul id="demo1">
-				<li>
-					<a href="#slide1">
-						<img src="images/IMG_0024.JPG" alt="image1"/>
-					</a>
-				</li>
-				<li>
-					<a href="#slide2">
-						<img src="images/IMG_0026.gif" alt="image1"/>
-					</a>
-				</li>
-				<li>
-					<a href="#slide3">
-						<img src="images/IMG_0290.JPG" alt="image1"/>
-					</a>
-				</li>
-			</ul>
-			</article>
-		</section>
+<div id="how_it_work">
 
-		<script>
-			$(function() {
-				var demo1 = $("#demo1").slippry({
-					 transition: 'fade',
-					 useCSS: true,
-					 speed: 1000,
-					 pause: 5000,
-					 auto: true,
-					 preload: 'visible',
-					 autoHover: false
-				});
+	<div class="flex flex_center">
+		<div class="flex flex_center" id="h_it_work">
+			 ЯК ЦЕ ПРАЦЮЄ
+		</div>
+	</div>
 
-			});
-		</script>
-	</body>
-</html>
+	<div class="flex flex_center">
+		<div class="flex flex_center flex_wrap column_h_i_w flex_align_start">
+			<div class="flex flex_center number_circle">
+				1
+			</div>
+			<div class="flex flex_center title_h_i_w">
+				Вибір
+			</div>
+			<div class="flex flex_center text_h_i_w">
+				Тут ви знайдете все, що тільки потрібно, щоб привітати дорогу вам людину, сповна висловити свої почуття або ж просто зробити день для когось трішки кращим. І з цим вам допоможе величезний асортимент квітів та композицій, що ми пропонуємо та чи малий список послуг, які наші працівники любязно вам запропонують.
+			</div>
+		</div>
+		<div class="flex flex_center flex_wrap column_h_i_w flex_align_start">
+			<div class="flex flex_center number_circle">
+				2
+			</div>
+			<div class="flex flex_center title_h_i_w">
+				Оплата
+			</div>
+			<div class="flex flex_center text_h_i_w">
+				Даний сайт зроблений максимально просто та доступно, щоб кожен без перешкод міг отримати найкраще. Скориставшись оплатою через банківську карту ви зможете отримати ваше замовлення не виходячи з домівки або ж розрахуватися готівкою.
+			</div>
+		</div>
+		<div class="flex flex_center flex_wrap column_h_i_w flex_align_start">
+			<div class="flex flex_center number_circle">
+				3
+			</div>
+			<div class="flex flex_center title_h_i_w">
+				Отримання
+			</div>
+			<div class="flex flex_center text_h_i_w">
+				Щоб отримати своє замовлення достатньо відвідати наш магазин або просто замовити доставку просто до вашого дому.
+			</div>
+		</div>
+	</div>
+
+
+</div>
